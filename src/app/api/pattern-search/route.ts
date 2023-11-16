01a11ff85b85e226b2f222aa4743e87842649651
Philippe Fanaro <philippefanaro@gmail.com>
2023-11-16 15:16:59 -0300

    minor clean up

--- src/app/api/pattern-search/route.ts
+++ src/app/api/pattern-search/route.ts
@@ -4,8 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { neo4jSession } from "@config/db";
 
-import { getAllNodes } from "@utils/neo4j_utils";
-
 const patternSearchReqBodySchema = z.object({
   pattern: z.array(z.string()),
 });
@@ -21,28 +19,12 @@ export async function POST(req: NextRequest) {
 
     const results = await neo4jSession.executeWrite((tx) =>
       tx.run(/* cypher */ `
-        // MATCH game_paths =   (:GameNode)
-        //                     -[:PLAYS_NEXT*1..]
-        //                    ->(:BoardNode)
-
-        MATCH   (g1:GameNode)
-               -[:PLAYS_FIRST]
-              ->(:BoardNode{ x: 'q', y: 'd' })
-        MATCH   (g2:GameNode)
-               -[:PLAYS_FIRST]
-              ->(:BoardNode)
-               -[:PLAYS_NEXT] 
-              ->(:BoardNode{ x: 'q', y: 'd' })
-           
-        RETURN g1, g2
       `)
     );
 
     console.log(results);
 
-    const nodes = getAllNodes(results);
-
-    return NextResponse.json({ nodes });
+    // return NextResponse.json({ nodes });
   } catch (e) {
     console.error(e);
 
