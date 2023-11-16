48cce3d9f814997a300e68c77d137778e01fe5ad
Philippe Fanaro <philippefanaro@gmail.com>
2023-11-16 09:34:42 -0300

    minor edit

--- src/app/lib/models/utils/sgf.ts
+++ src/app/lib/models/utils/sgf.ts
@@ -29,33 +29,33 @@ export type GameNodeData = Pick<SgfData, "AB" | "AW">;
  */
 export type SgfData = {
   // 1. Metadata
-  GM?: ["1"]; // Game Type (GM = '1' is Go)
-  FF?: [string]; // File Format
-  CA?: [string]; // Character Set
-  AP?: [string]; // Application used to produce the file
+  GM?: ["1"];     // Game Type (GM = '1' is Go)
+  FF?: [string];  // File Format
+  CA?: [string];  // Character Set
+  AP?: [string];  // Application used to produce the file
   // 2. Game Info
-  KM?: [string]; // Komi
-  SZ?: [string]; // Board Size
-  DT?: [string]; // Date
-  HA?: [string]; // Number of Handicap Stones
-  RU?: [string]; // Rules Set in Use
-  GN?: [string]; // Game Name
-  EV?: [string]; // Event
+  KM?: [string];  // Komi
+  SZ?: [string];  // Board Size
+  DT?: [string];  // Date
+  HA?: [string];  // Number of Handicap Stones
+  RU?: [string];  // Rules Set in Use
+  GN?: [string];  // Game Name
+  EV?: [string];  // Event
   // 3. Players
-  PB?: [string]; // Black Player
-  BR?: [string]; // Black's Rating
-  PW?: [string]; // White Player
-  WR?: [string]; // White's Rating
+  PB?: [string];  // Black Player
+  BR?: [string];  // Black's Rating
+  PW?: [string];  // White Player
+  WR?: [string];  // White's Rating
   // 4. Comments
-  C?: [string]; // (Move) Comments
-  GC?: [string]; // Game Comment
+  C?: [string];   // (Move) Comments
+  GC?: [string];  // Game Comment
   // 5. Editing the Goban
   PL?: [Players]; // Who plays next
-  AB?: string[]; // Add Black stones
-  AW?: string[]; // Add White stones
+  AB?: string[];  // Add Black stones
+  AW?: string[];  // Add White stones
   // 6. Moves
-  B?: [string]; // What Black plays
-  W?: [string]; // What White Plays
+  B?: [string];   // What Black plays
+  W?: [string];   // What White Plays
 };
 
 export type GameTreeNodeId = number;
