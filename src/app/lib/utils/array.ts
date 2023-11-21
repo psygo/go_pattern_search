63d83e2e1632c21ae97c3c99aba142626c1d474f
Philippe Fanaro <philippefanaro@gmail.com>
2023-11-21 14:18:29 -0300

    adding more useful Array utilities

--- src/app/lib/utils/array.ts
+++ src/app/lib/utils/array.ts
@@ -1,13 +1,37 @@
 export {};
 
+/**
+ * Extra Utilities on `Array`
+ */
 declare global {
   interface Array<T> {
+    // 1. Items
     first(): T;
     second(): T;
     penultimate(): T;
     last(): T;
+
+    // 2. Optional Methods
+    optionalMap(
+      callbackfn: (
+        value: T,
+        index: number,
+        array: T[]
+      ) => T,
+      mapOrNot: boolean
+    ): Array<T>;
+    optionalFlatMap(
+      callbackfn: (
+        value: T,
+        index: number,
+        array: T[]
+      ) => T,
+      mapOrNot: boolean
+    ): Array<T>;
   }
 }
+
+// 1. Items
 
 Array.prototype.first = function <T>(): T {
   return this.at(0);
@@ -23,4 +47,20 @@ Array.prototype.penultimate = function <T>(): T {
 
 Array.prototype.last = function <T>(): T {
   return this.at(-1);
+};
+
+// 2. Optional Methods
+
+Array.prototype.optionalMap = function <T>(
+  callbackfn: (value: T, index: number, array: T[]) => T,
+  mapOrNot: boolean
+): Array<T> {
+  return mapOrNot ? this.map(callbackfn) : this;
+};
+
+Array.prototype.optionalFlatMap = function <T>(
+  callbackfn: (value: T, index: number, array: T[]) => T,
+  mapOrNot: boolean
+): Array<T> {
+  return mapOrNot ? this.flatMap(callbackfn) : this;
 };
