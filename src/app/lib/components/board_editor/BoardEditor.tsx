6c7fad120ce80bec3b1711d8badd80bf3bb5a8c9
Philippe Fanaro <philippefanaro@gmail.com>
2023-11-22 12:15:58 -0300

    able to draw a grid

--- src/app/lib/components/board_editor/BoardEditor.tsx
+++ src/app/lib/components/board_editor/BoardEditor.tsx
@@ -27,7 +27,34 @@ export function BoardEditor() {
 
   function setupGrid() {
     const gridCanvas = gridCanvasRef.current!;
-    const gridCtx = gridCanvas.getContext("2d");
+    const gridCtx = gridCanvas.getContext("2d")!;
+    const p = 10;
+
+    gridCanvas.style.width = width + "px";
+    gridCanvas.style.height = height + "px";
+
+    // Otherwise we get blurry lines
+    // Referenece: [Stack Overflow - Canvas drawings, like lines, are blurry](https://stackoverflow.com/a/59143499/4756173)
+    const scale = window.devicePixelRatio;
+
+    gridCanvas.width = width * scale;
+    gridCanvas.height = height * scale;
+
+    gridCtx.scale(scale, scale);
+
+    const step = width / 19;
+    for (let x = 0; x <= width; x += step) {
+      gridCtx.moveTo(0.5 + x + p, p);
+      gridCtx.lineTo(0.5 + x + p, height + p);
+    }
+
+    for (let x = 0; x <= height; x += step) {
+      gridCtx.moveTo(p, 0.5 + x + p);
+      gridCtx.lineTo(width + p, 0.5 + x + p);
+    }
+
+    gridCtx.strokeStyle = "black";
+    gridCtx.stroke();
   }
 
   useEffect(() => {
@@ -38,10 +65,10 @@ export function BoardEditor() {
   return (
     <Box sx={{ position: "relative" }}>
       <canvas
+        width={width}
+        height={height}
         ref={stonesCanvasRef}
         style={{
-          width,
-          height,
           position: "absolute",
           zIndex: 2,
         }}
@@ -50,8 +77,6 @@ export function BoardEditor() {
       <canvas
         ref={gridCanvasRef}
         style={{
-          width,
-          height,
           position: "absolute",
           zIndex: 1,
         }}
@@ -59,9 +84,9 @@ export function BoardEditor() {
       ></canvas>
       <canvas
         ref={backgroundCanvasRef}
+        width={width}
+        height={height}
         style={{
-          width,
-          height,
           position: "absolute",
           zIndex: 0,
         }}
